27d76a37f745bab2f01e5f1d0c560bbaace4fe11
marcobarlo <65128997+marcobarlo@users.noreply.github.com>
2020-11-29 20:41:02 +0100

    Add files via upload

---
+++ Greenhouse-1/src/test/java/com/tdd/demo/Greenhouse1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tdd.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Greenhouse1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
